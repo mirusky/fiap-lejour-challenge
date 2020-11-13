90e0318bf8363288da26fb8b3246833f2b940d1b
João Victor Santos <profissional.joaovss@gmail.com>
2020-11-13 23:50:24 +0000

    feat: populating database

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import { createConnection, getManager } from "typeorm";
 import express from "express";
 import bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
 import Axios from "axios";
+import { User } from "./entity/User";
+import { Appointment } from "./entity/Appointment";
+import { Favorite } from "./entity/Favorite";
+import { Invoice } from "./entity/Invoice";
+import { Wedding } from "./entity/Wedding";
 
-// const BASEURL = "https://sheet2api.com/v1/ByR2h1huRjyQ/fiap/"
-// Axios.get(BASEURL +"/user").then((r) => {
-//   const result = r.data as User[];
-//   console.log(result);
-// });
+const BASEURL = "https://sheet2api.com/v1/ByR2h1huRjyQ/fiap";
 
 createConnection()
   .then(async (connection) => {
@@ -42,15 +42,57 @@ createConnection()
       );
     });
 
-    // setup express app here
-    // ...
+    // Populating database with data
+    getManager()
+      .findOneOrFail(Appointment)
+      .catch((e) => {
+        Axios.get(BASEURL + "/appointment").then((result) => {
+          const appointments = result.data as Appointment[];
+          getManager().save(Appointment, appointments);
+        });
+      });
+    getManager()
+      .findOneOrFail(Favorite)
+      .catch((e) => {
+        Axios.get(BASEURL + "/wedding_favorites").then((result) => {
+          const favorites = result.data as Favorite[];
+          getManager().save(Favorite, favorites);
+        });
+      });
+    getManager()
+      .findOneOrFail(Invoice)
+      .catch((e) => {
+        Axios.get(BASEURL + "/invoice").then((result) => {
+          const invoices = result.data as Invoice[];
+          getManager().save(Invoice, invoices);
+        });
+      });
+    getManager()
+      .findOneOrFail(Wedding)
+      .catch((e) => {
+        Axios.get(BASEURL + "/wedding").then((result) => {
+          let weddings = result.data as Wedding[];
+          weddings.map((w) => { // HACK: Normalizing some fields since it's not normalized 
+            w.BUDGET = w.BUDGET.toString() == "NULL" ? null : w.BUDGET;
+            w.WEDDING_DATE = w.WEDDING_DATE == "NULL" ? null : w.WEDDING_DATE;
+            w.NUMBER_OF_GUESTS =
+              typeof w.NUMBER_OF_GUESTS == "number" ? w.NUMBER_OF_GUESTS : null;
+            return w;
+          });
+          getManager().save(Wedding, weddings);
+        });
+      });
+    getManager()
+      .findOneOrFail(User)
+      .catch((e) => {
+        Axios.get(BASEURL + "/user").then((result) => {
+          const users = result.data as User[];
+          getManager().save(User, users);
+        });
+      });
 
     // start express server
     app.listen(3000);
-
-    // TODO: Check if exist data in database
-    // If dont exist nothing populate, requesting data from https://sheet2api.com/v1/ByR2h1huRjyQ/fiap/
-    // Otherwise do nothing
 
     console.log(
       "Express server has started on port 3000. Open http://localhost:3000/users to see results"
